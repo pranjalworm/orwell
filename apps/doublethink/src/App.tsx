bada52a94d3c4f72f5cf3e87bd07a78b317f9354
Pranjal Dubey <hi@pranjaldubey.com>
2026-05-20 23:41:20 +0200

    basic /GET books integration

--- apps/doublethink/src/App.tsx
+++ apps/doublethink/src/App.tsx
@@ -1,23 +1,28 @@
-import { BrowserRouter, Routes, Route } from 'react-router'
-import CataloguePage from './pages/CataloguePage'
-import BookDetailPage from './pages/BookDetailPage'
+import { BrowserRouter, Routes, Route } from "react-router";
+import CataloguePage from "./pages/CataloguePage";
+import BookDetailPage from "./pages/BookDetailPage";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<CataloguePage />} />
-        <Route path="/book/:id" element={<BookDetailPage />} />
-        <Route path="/shelf" element={<CataloguePage />} />
-        <Route path="/reading-now" element={<CataloguePage />} />
-        <Route path="/fiction" element={<CataloguePage />} />
-        <Route path="/non-fiction" element={<CataloguePage />} />
-        <Route path="/poetry" element={<CataloguePage />} />
-        <Route path="/essays" element={<CataloguePage />} />
-        <Route path="/history" element={<CataloguePage />} />
-      </Routes>
-    </BrowserRouter>
-  )
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<CataloguePage />} />
+          <Route path="/book/:id" element={<BookDetailPage />} />
+          <Route path="/shelf" element={<CataloguePage />} />
+          <Route path="/reading-now" element={<CataloguePage />} />
+          <Route path="/fiction" element={<CataloguePage />} />
+          <Route path="/non-fiction" element={<CataloguePage />} />
+          <Route path="/poetry" element={<CataloguePage />} />
+          <Route path="/essays" element={<CataloguePage />} />
+          <Route path="/history" element={<CataloguePage />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
+  );
 }
 
-export default App
+export default App;
